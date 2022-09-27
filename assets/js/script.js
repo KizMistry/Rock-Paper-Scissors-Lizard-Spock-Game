71b64836d9b8bc11a9fd4a3d456838747bb87f56
KizMistry <Kiz_mistry@hotmail.com>
2022-09-27 13:31:07 +0000

    add ids to pick your hand divs and added event listeners to corresponding divs, and created function to add inner html to user pick div when hands div is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-            let hands = document.getElementsByClassName("hands");
-
-            for (let hand of hands) {
-                button.addEventListener("click", runGame() {
-                    if (this.getAttribute("data-type") === "submit") {
-                        play();
-                    } else {
-                        let gameType = this.getAttribute("data-type");
-                        runGame(gameType);
-                    }
-                });
-            }
-        
-            runGame(gameType);
-        };
-
 
             /**
              * The main game "loop", called when the script is first loaded
              * and after the user's answer has been processed
              */
-            function runGame(gameType) {
-
-                if (gameType === "rock") {
-                    rock();
-                } else if (gameType === "paper") {
-                    paper();
-                } else if (gameType === "scissors") {
-                    scissors();
-                } else if (gameType === "lizard") {
-                    lizard();
-                }  else if (gameType === "spock") {
-                    spock();
-                }  else {
-                    alert(`Unknown game type: ${gameType}`);
-                    throw `Unknown game type: ${gameType}. Aborting!`;
-                }
-
-            }
+        document.getElementById('rock').addEventListener('click', rock)
+        document.getElementById('paper').addEventListener('click', paper)
+        document.getElementById('scissors').addEventListener('click', scissors)
+        document.getElementById('lizard').addEventListener('click', lizard)
+        document.getElementById('spock').addEventListener('click', spock)
 
             function rock() {
                 document.getElementById('users-pick').innerHTML= `<img src="assets/images/rock-hand-icon.png" alt="rock hand icon">`
