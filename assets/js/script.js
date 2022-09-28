afba5e16ba0657c9d5188b5461f2a2b307b43ec5
KizMistry <Kiz_mistry@hotmail.com>
2022-09-28 14:59:42 +0000

    add a play function that will generate a random hand to play against when the play button is clicked, an alert will appear if the user has not picked first

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@
         document.getElementById('scissors').addEventListener('click', scissors)
         document.getElementById('lizard').addEventListener('click', lizard)
         document.getElementById('spock').addEventListener('click', spock)
+        document.getElementById('play').addEventListener('click', play)
 
             function rock() {
                 document.getElementById('users-pick').innerHTML= `<img src="assets/images/rock-hand-icon.png" alt="rock hand icon">`
@@ -30,11 +31,28 @@
                 document.getElementById('random-pick').innerHTML= `<img src="assets/images/spock-hand-icon.png" alt="spock hand icon">`
             }
 
-            function play() {
-                console.log("Hello")
-            }
-
             /** need event listener for play button click and run next function.
              * need function to create random hand and change inner html of random-pick div.
              * 
+             */
+
+             function play() {
+                
+                let userPick = document.getElementById('users-pick')
+                let hands = document.getElementsByClassName('hands-img')
+
+                // random number between 1 and 5
+                let randomHand = Math.floor(Math.random() * 5)
+
+                if (userPick.innerHTML !== "") {
+                    document.getElementById('random-pick').innerHTML= hands[randomHand].innerHTML
+                } else {
+                    alert('Please select a hand')
+                }
+                
+                
+            }
+
+            /** need a function to check the winner and create a 'you win/lose/draw' message.
+             * need a function to then increment the relevant score.
              */
