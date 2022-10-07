f6268feac3703a35c4bdcbe33434c02b2c934a10
KizMistry <Kiz_mistry@hotmail.com>
2022-10-07 12:45:43 +0000

    added global variables for win, draw, lose and updated the functions using the new variables in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ let lizardHand = document.getElementById('lizard');
 let spockHand = document.getElementById('spock');
 let usersPick = document.getElementById('users-pick');
 let randomsPick = document.getElementById('random-pick');
+let rulesModal = document.getElementById("rulesModal");
+let rulesButton = document.getElementById("rules-button");
+let closeModal = document.querySelector('span');
+let win = document.getElementById("won");
+let draw = document.getElementById("drawn");
+let lose = document.getElementById("lost");
 
 // Event listeners for game buttons to run each function
 
@@ -23,61 +29,61 @@ document.getElementById('reset-button').addEventListener('click', reset);
 function rock() {
     usersPick.innerHTML = `<img data-type="rock" src="assets/images/rock-hand-icon.png" alt="rock hand icon">`;
     randomsPick.innerHTML = "";
-    rockHand.style.borderColor= "green";
-    paperHand.style.borderColor= "lightgrey";
-    scissorHand.style.borderColor= "lightgrey";
-    lizardHand.style.borderColor= "lightgrey";
-    spockHand.style.borderColor= "lightgrey";
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
+    rockHand.style.borderColor = "green";
+    paperHand.style.borderColor = "lightgrey";
+    scissorHand.style.borderColor = "lightgrey";
+    lizardHand.style.borderColor = "lightgrey";
+    spockHand.style.borderColor = "lightgrey";
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
 }
 
 function paper() {
     usersPick.innerHTML = `<img data-type="paper" src="assets/images/paper-hand-icon.png" alt="paper hand icon">`;
     randomsPick.innerHTML = "";
-    paperHand.style.borderColor= "green";
-    rockHand.style.borderColor= "lightgrey";
-    scissorHand.style.borderColor= "lightgrey";
-    lizardHand.style.borderColor= "lightgrey";
-    spockHand.style.borderColor= "lightgrey";
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
+    paperHand.style.borderColor = "green";
+    rockHand.style.borderColor = "lightgrey";
+    scissorHand.style.borderColor = "lightgrey";
+    lizardHand.style.borderColor = "lightgrey";
+    spockHand.style.borderColor = "lightgrey";
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
 }
 
 function scissors() {
     usersPick.innerHTML = `<img data-type="scissors" src="assets/images/scissors-hand-icon.png" alt="scissors hand icon">`;
     randomsPick.innerHTML = "";
-    scissorHand.style.borderColor= "green";
-    rockHand.style.borderColor= "lightgrey";
-    paperHand.style.borderColor= "lightgrey";
-    lizardHand.style.borderColor= "lightgrey";
-    spockHand.style.borderColor= "lightgrey";
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
+    scissorHand.style.borderColor = "green";
+    rockHand.style.borderColor = "lightgrey";
+    paperHand.style.borderColor = "lightgrey";
+    lizardHand.style.borderColor = "lightgrey";
+    spockHand.style.borderColor = "lightgrey";
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
 }
 
 function lizard() {
     usersPick.innerHTML = `<img data-type="lizard" src="assets/images/lizard-hand-icon.png" alt="lizard hand icon">`;
     randomsPick.innerHTML = "";
-    lizardHand.style.borderColor= "green";
-    rockHand.style.borderColor= "lightgrey";
-    scissorHand.style.borderColor= "lightgrey";
-    paperHand.style.borderColor= "lightgrey";
-    spockHand.style.borderColor= "lightgrey";
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
+    lizardHand.style.borderColor = "green";
+    rockHand.style.borderColor = "lightgrey";
+    scissorHand.style.borderColor = "lightgrey";
+    paperHand.style.borderColor = "lightgrey";
+    spockHand.style.borderColor = "lightgrey";
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
 }
 
 function spock() {
     usersPick.innerHTML = `<img data-type="spock" src="assets/images/spock-hand-icon.png" alt="spock hand icon">`;
     randomsPick.innerHTML = "";
-    spockHand.style.borderColor= "green";
-    rockHand.style.borderColor= "lightgrey";
-    scissorHand.style.borderColor= "lightgrey";
-    lizardHand.style.borderColor= "lightgrey";
-    paperHand.style.borderColor= "lightgrey";
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
+    spockHand.style.borderColor = "green";
+    rockHand.style.borderColor = "lightgrey";
+    scissorHand.style.borderColor = "lightgrey";
+    lizardHand.style.borderColor = "lightgrey";
+    paperHand.style.borderColor = "lightgrey";
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
 }
 
 // Function to generate a random hand when play button is clicked
@@ -99,6 +105,10 @@ function play() {
 
 }
 
+/** Function to check users pick against random pick and determine
+ *  the outcome and return correct corresponding result
+ */
+
 function checkResult() {
 
     let userPick = document.getElementById("users-pick").children[0];
@@ -109,76 +119,76 @@ function checkResult() {
 
     if (usersHand === randomsHand) {
         incrementDrawn();
-        message.innerHTML= "Stalemate... You picked the same hands!";
+        message.innerHTML = "Stalemate... You picked the same hands!";
     } else if (usersHand === "rock") {
         if (randomsHand === "paper") {
             incrementLost();
-            message.innerHTML= "You Lose... Paper covers Rock!";
+            message.innerHTML = "You Lose... Paper covers Rock!";
         } else if (randomsHand === "spock") {
             incrementLost();
-            message.innerHTML= "You Lose... Spock vaporizes Rock!";
+            message.innerHTML = "You Lose... Spock vaporizes Rock!";
         } else if (randomsHand === "scissors") {
             incrementWin();
-            message.innerHTML= "You Win... Rock smashes Scissors!";
+            message.innerHTML = "You Win... Rock smashes Scissors!";
         } else if (randomsHand === "lizard") {
             incrementWin();
-            message.innerHTML= "You Win... Rock crushes Lizard!";
+            message.innerHTML = "You Win... Rock crushes Lizard!";
         }
     } else if (usersHand === "paper") {
         if (randomsHand === "lizard") {
             incrementLost();
-            message.innerHTML= "You Lose... Lizard eats Paper!";
+            message.innerHTML = "You Lose... Lizard eats Paper!";
         } else if (randomsHand === "scissors") {
             incrementLost();
-            message.innerHTML= "You Lose... scissors cuts Paper!";
+            message.innerHTML = "You Lose... scissors cuts Paper!";
         } else if (randomsHand === "rock") {
             incrementWin();
-            message.innerHTML= "You Win... Paper covers Rock!";
+            message.innerHTML = "You Win... Paper covers Rock!";
         } else if (randomsHand === "spock") {
             incrementWin();
-            message.innerHTML= "You Win... Paper disproves Spock!";
+            message.innerHTML = "You Win... Paper disproves Spock!";
         }
     } else if (usersHand === "scissors") {
         if (randomsHand === "rock") {
             incrementLost();
-            message.innerHTML= "You Lose... Rock crushes Scissors!";
+            message.innerHTML = "You Lose... Rock crushes Scissors!";
         } else if (randomsHand === "spock") {
             incrementLost();
-            message.innerHTML= "You Lose... Spock smashes Scissors!";
+            message.innerHTML = "You Lose... Spock smashes Scissors!";
         } else if (randomsHand === "paper") {
             incrementWin();
-            message.innerHTML= "You Win... Scissors cuts Paper!";
+            message.innerHTML = "You Win... Scissors cuts Paper!";
         } else if (randomsHand === "lizard") {
             incrementWin();
-            message.innerHTML= "You Win... Scissors decapitates Lizard!";
+            message.innerHTML = "You Win... Scissors decapitates Lizard!";
         }
-     } else if (usersHand === "lizard") {
+    } else if (usersHand === "lizard") {
         if (randomsHand === "rock") {
             incrementLost();
-            message.innerHTML= "You Lose... Rock crushes Lizard!";
+            message.innerHTML = "You Lose... Rock crushes Lizard!";
         } else if (randomsHand === "scissors") {
             incrementLost();
-            message.innerHTML= "You Lose... Scissors decapitates Lizard!";
+            message.innerHTML = "You Lose... Scissors decapitates Lizard!";
         } else if (randomsHand === "paper") {
             incrementWin();
-            message.innerHTML= "You Win... Lizard eats Paper!";
+            message.innerHTML = "You Win... Lizard eats Paper!";
         } else if (randomsHand === "spock") {
             incrementWin();
-            message.innerHTML= "You Win... Lizard poisons Spock!";
+            message.innerHTML = "You Win... Lizard poisons Spock!";
         }
-     } else if (usersHand === "spock") {
+    } else if (usersHand === "spock") {
         if (randomsHand === "lizard") {
             incrementLost();
-            message.innerHTML= "You Lose... Lizard poisons Spock!";
+            message.innerHTML = "You Lose... Lizard poisons Spock!";
         } else if (randomsHand === "paper") {
             incrementLost();
-            message.innerHTML= "You Lose... Paper disproves Spock!";
+            message.innerHTML = "You Lose... Paper disproves Spock!";
         } else if (randomsHand === "rock") {
             incrementWin();
-            message.innerHTML= "You Win... Spock vaporizes Rock!";
+            message.innerHTML = "You Win... Spock vaporizes Rock!";
         } else if (randomsHand === "scissors") {
             incrementWin();
-            message.innerHTML= "You Win... Spock smashes Scissors!";
+            message.innerHTML = "You Win... Spock smashes Scissors!";
         }
     }
 
@@ -186,78 +196,70 @@ function checkResult() {
     console.log(randomsHand);
 }
 
-/**
- * Gets the current rounds won from the DOM and increments it by 1
- */
+// Gets the current rounds won from the DOM and increments it by 1
+
 function incrementWin() {
 
-    let oldScore = parseInt(document.getElementById("won").innerText);
-    document.getElementById("won").innerText = ++oldScore;
-    usersPick.style.borderColor= "green";
-    randomsPick.style.borderColor= "red";
+    let oldScore = parseInt(win.innerText);
+    win.innerText = ++oldScore;
+    usersPick.style.borderColor = "green";
+    randomsPick.style.borderColor = "red";
 
 }
 
-/**
- * Gets the current rounds drawn from the DOM and increments it by 1
- */
- function incrementDrawn() {
+// Gets the current rounds drawn from the DOM and increments it by 1
 
-    let oldScore = parseInt(document.getElementById("drawn").innerText);
-    document.getElementById("drawn").innerText = ++oldScore;
-    usersPick.style.borderColor= "grey";
-    randomsPick.style.borderColor= "grey";
+function incrementDrawn() {
+
+    let oldScore = parseInt(draw.innerText);
+    draw.innerText = ++oldScore;
+    usersPick.style.borderColor = "grey";
+    randomsPick.style.borderColor = "grey";
 
 }
 
-/**
- * Gets the current rounds lost from the DOM and increments it by 1
- */
+// Gets the current rounds lost from the DOM and increments it by 1
+
 function incrementLost() {
 
-    let oldScore = parseInt(document.getElementById("lost").innerText);
-    document.getElementById("lost").innerText = ++oldScore;
-    usersPick.style.borderColor= "red";
-    randomsPick.style.borderColor= "green";
+    let oldScore = parseInt(lose.innerText);
+    lose.innerText = ++oldScore;
+    usersPick.style.borderColor = "red";
+    randomsPick.style.borderColor = "green";
 
 }
 
 // Function to reset the game and scores
+
 function reset() {
     let hands = document.getElementsByClassName('hands');
 
     for (var i = 0; i < hands.length; i++) {
-        hands[i].style.borderColor= "lightgrey";
-     }
-    usersPick.style.borderColor= "beige";
-    randomsPick.style.borderColor= "beige";
-    document.getElementById("won").innerText = "0";
-    document.getElementById("drawn").innerText = "0";
-    document.getElementById("lost").innerText = "0";
+        hands[i].style.borderColor = "lightgrey";
+    }
+    usersPick.style.borderColor = "beige";
+    randomsPick.style.borderColor = "beige";
+    win.innerText = "0";
+    draw.innerText = "0";
+    lose.innerText = "0";
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// Open the modal when how to play button clicked
 
-// Get the button that opens the modal
-var btn = document.getElementById("rules-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+rulesButton.onclick = function () {
+    rulesModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// Close the modal when 'x' button is clicked
+
+closeModal.onclick = function () {
+    rulesModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// Modal closed when anywhere outside the modal is clicked
+
+window.onclick = function (event) {
+    if (event.target == rulesModal) {
+        rulesModal.style.display = "none";
+    }
 }
